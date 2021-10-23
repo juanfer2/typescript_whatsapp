924c3509ea2409f29246fb6d1377c1589b947686
juanfer2 <jfvilladiego3@gmail.com>
2021-10-23 18:43:02 -0500

    Deploy

--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,6 @@ export const server = Server.init((process.env.PORT as unknown) as number);
 server.start(async() =>{
   logger.info(`🚀 Server is running at PORT: ${process.env.PORT}`)
   logger.info(`🚀 GraphQL is now running on http://localhost:${process.env.PORT}/graphql`)
+
   //startJobs()
 });
